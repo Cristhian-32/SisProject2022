98085b0b3a0f6d416c74268e4ff43fbe8c2ba59d
Andrepaolo <andrepaol32@gmail.com>
2022-11-07 21:46:50 -0500

    Migraciones y data base

---
+++ database/migrations/2022_11_08_023427_create_researchs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('research', function (Blueprint $table) {
+            $table->id();
+            $table->string('fecha');
+            $table->string('fecha_inicio');
+            $table->string('fecha_final');
+            $table->string('title');
+            $table->string('path');
+            $table->string('calification');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('research');
+    }
+};
