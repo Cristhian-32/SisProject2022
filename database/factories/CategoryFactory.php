c91cd11cf1586516a7999744192a6cfd669ddc08
aldhairH <aldhair.huaquisto@upeu.edu.pe>
2022-11-07 23:08:16 -0500

    factories

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Dotenv\Util\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use function Livewire\str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $name=$this->faker->unique()->word(20);
+        return [
+            'name'=>$name,
+
+            //
+        ];
+    }
+}
---
+++ database/factories/PersonFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Person>
+ */
+class PersonFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'male lastname'=>$this->faker->lastname(),
+            'female lastname'=>$this->faker->lastName(),
+            'code'=>$this->faker->unique->code()
+            //
+        ];
+    }
+}
